25599fd91bfc7ccfb8dfc106054abef408dedd85
Maciej Sitko <maciejsitko@Maciejs-MBP.home>
2018-02-11 15:21:29 +0000

    Updated for the test folder

---
+++ test/testSetup.js
@@ -0,0 +1,9 @@
+import { configure, shallow, render, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({ adapter: new Adapter() })
+
+// enzyme
+global.shallow = shallow
+global.render = render
+global.mount = mount
