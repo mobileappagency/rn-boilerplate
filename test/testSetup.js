bd1a2885f58b8708fabb4329589371bd13e7a0ea
Maciej Sitko <maciejsitko@Maciejs-MBP.home>
2018-02-12 22:58:48 +0000

    Some globals added

--- test/testSetup.js
+++ test/testSetup.js
@@ -1,9 +1,4 @@
-import { configure, shallow, render, mount } from 'enzyme'
+import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
-
-// enzyme
-global.shallow = shallow
-global.render = render
-global.mount = mount
